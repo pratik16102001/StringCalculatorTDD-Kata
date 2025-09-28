9671f11cb6fc803627dab20296c0c4c938a6dc3c
pratik16102001 <pratikvaishnani1610@gmail.com>
2025-09-28 13:57:54 +0530

    Green: Update add method to handle single number input

--- calculator_test.py
+++ calculator_test.py
@@ -8,3 +8,10 @@ class TestStringCalculator:
 
     def test_empty_string_returns_zero(self):
         assert self.calc.add("") == 0
+
+    def test_single_number_returns_itself(self):
+        assert self.calc.add("1") == 1
+        assert self.calc.add("3") == 3
+        assert self.calc.add("5") == 5
+
+        
