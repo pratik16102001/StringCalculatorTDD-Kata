1168c767503e4918108f4886892b5b5ef0536621
pratik16102001 <pratikvaishnani1610@gmail.com>
2025-09-28 14:03:06 +0530

    Green: Extend add method to handle two comma-separated numbers

--- calculator.py
+++ calculator.py
@@ -7,6 +7,11 @@ class StringCalculatorTDDKata:
     def add(numbers: str) -> int:
         if numbers == "":
             return 0
+
+        if "," in numbers:
+            parts = numbers.split(",")
+            return int(parts[0]) + int(parts[1])
+
         return int(numbers)
         
     
