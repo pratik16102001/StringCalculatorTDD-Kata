9e6069d529d4166155a5a2dc2bc7f7f70b740a51
pratik16102001 <pratikvaishnani1610@gmail.com>
2025-09-28 14:25:23 +0530

    Green: Update add method to support the custom delimiter r

--- calculator.py
+++ calculator.py
@@ -8,7 +8,13 @@ class StringCalculatorTDDKata:
         if numbers == "":
             return 0
 
-        parts = re.split(r',|\n', numbers)
-        return sum(int(x) for x in parts)
+        delimiter = ",|\n"
+        if numbers.startswith("//"):
+            parts = numbers.split("\n", 1)
+            delimiter = re.escape(parts[0][2:])
+            numbers = parts[1]
+
+        num_list = re.split(delimiter + "|,|\n", numbers)
+        return sum(int(x) for x in num_list)
         
     
